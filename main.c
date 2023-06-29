54fabb2d1f6d65473a5427528669400931a8b0f5
Ryann Graham <r.m.graham@gmail.com>
2023-06-29 13:38:43 -0700

    go: lints & cleanup
    Remove dead code, make 'go list' happy.

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:build ignore
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
